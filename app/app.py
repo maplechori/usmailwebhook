544605fe6052045fafa15cefe43ae00fba5b8643
Adrian Montero <chori@hotmail.com>
2024-10-04 21:13:12 -0700

    added specific camera string detection

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 import tempfile
-
 import torch
 import cv2
 import requests
@@ -8,9 +7,12 @@ import logging
 import pathlib
 import platform
 import shutil
+import warnings
 
 from huggingface_hub import hf_hub_download
 from flask import Flask, request, Response, json
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 app = Flask(__name__)
 
@@ -20,71 +22,87 @@ logger = logging.getLogger('USMailDetect')
 logger.setLevel(logging.INFO)
 
 
-@app.route('/', methods=['POST'] )
+@app.route('/', methods=['POST'])
 def detect_usmail():
+    logger.info("DETECT USMAIL")
+    url = request.json.get('url')
 
-	logger.info("DETECT USMAIL")
-	url = request.json.get('url', None)
+    if not url:
+        return Response(
+            response=json.dumps({
+                'message': 'No URL present'
+            }),
+            status=200,
+            mimetype='application/json'
+        )
 
-	if not url:
-		return "No URL"
+    desc = request.json.get('desc')
+    if 'Garage' not in desc:
+        logger.info('Motion detected by different camera')
+        return Response(
+            response=json.dumps({
+                'message': 'Service only available for Garage camera'
+            }),
+            status=200,
+            mimetype='application/json'
+        )
 
-	plt = platform.system()
-	if plt == 'Windows':
-		pathlib.PosixPath = pathlib.WindowsPath
-	else:
-		pathlib.WindowsPath = pathlib.PosixPath
+    plt = platform.system()
+    if plt == 'Windows':
+        pathlib.PosixPath = pathlib.WindowsPath
+    else:
+        pathlib.WindowsPath = pathlib.PosixPath
 
-	logger.info(f"Downloading image {url}")
-	response = requests.get(url, stream=True)
+    logger.info(f"Downloading image {url}")
+    response = requests.get(url, stream=True)
 
-	if not url.endswith('jpg'):
-		return Response(
-			response=json.dumps({
-				'message': 'This service only accepts JPEG images'
-			}),
-			status=200,
-			mimetype='application/json'
-		)
+    if not url.endswith('jpg'):
+        return Response(
+            response=json.dumps({
+                'message': 'This service only accepts JPEG images'
+            }),
+            status=200,
+            mimetype='application/json'
+        )
 
-	temporary_file = tempfile.NamedTemporaryFile(delete=False)
+    temporary_file = tempfile.NamedTemporaryFile(delete=False)
 
-	with open(temporary_file.name, "wb") as jpg:
-		logger.info(f"Writing temporary file: {jpg.name}")
-		shutil.copyfileobj(response.raw, jpg)
-		temporary_file.close()
-		del response
+    with open(temporary_file.name, "wb") as jpg:
+        logger.info(f"Writing temporary file: {jpg.name}")
+        shutil.copyfileobj(response.raw, jpg)
+        temporary_file.close()
+        del response
 
-	us_mail_path = hf_hub_download(repo_id="maplechori/usmaildetection", filename="usmail.pt")
-	model = torch.hub.load('ultralytics/yolov5', 'custom', path=us_mail_path)
+    us_mail_path = hf_hub_download(repo_id="maplechori/usmaildetection", filename="usmail.pt")
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=us_mail_path)
 
-	img = cv2.imread(temporary_file.name)
-	os.unlink(temporary_file.name)
+    img = cv2.imread(temporary_file.name)
+    os.unlink(temporary_file.name)
 
-	if img is not None and img.any():
-		results = model(img)
+    if img is not None and img.any():
+        results = model(img)
 
-		detections = results.pandas().xyxy[0]
+        detections = results.pandas().xyxy[0]
 
-		if not detections.empty:
-			df = detections.loc[detections["name"] == "us_mail_symbol"]
-			if not df.empty and df["confidence"].iloc[0] > 0.20:
-				logger.info("Found US Mail symbol!")
-				confidence = df["confidence"].iloc[0]
+        if not detections.empty:
+            df = detections.loc[detections["name"] == "us_mail_symbol"]
+            if not df.empty and df["confidence"].iloc[0] > 0.20:
+                logger.info("Found US Mail symbol!")
+                confidence = df["confidence"].iloc[0]
 
-				return Response(response=json.dumps({
-					'message': f'US mail symbol identified on image with {confidence} confidence'
-				}),
-				status=200,
-				mimetype='application/json')
+                return Response(response=json.dumps({
+                    'message': f'US mail symbol identified on image with {confidence} confidence'
+                }),
+                    status=200,
+                    mimetype='application/json')
 
-	return Response(response=json.dumps({
-			'message': 'Unable to identify object in file'
-		}),
-		status=200,
-		mimetype='application/json'
-	)
+    return Response(response=json.dumps({
+        'message': 'Unable to identify object in file'
+    }),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=4343)
+    app.run(host='0.0.0.0', port=4343)
